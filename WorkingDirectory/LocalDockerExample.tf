b5a11c50fdb99ec127fe09981f7dedcfca610e6a
glcapps <58761442+glcapps@users.noreply.github.com>
2023-03-28 13:49:49 -0500

    docker early thoughts

---
+++ WorkingDirectory/LocalDockerExample.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers {
+        docker = {
+            source  = "kreuzwerker/docker"
+            version = "~> 2.17.0"
+        }
+    }
+}
+resource "docker_container" "rbitrarylabel" {
+
+}
